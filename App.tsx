4b24d6b9610758308b4557b727892e02cabf90f9
phprograming <paulo_hbs17@hotmail.com>
2022-03-20 12:02:58 -0300

    feat: config ci/cd with AppCenter

--- App.tsx
+++ App.tsx
@@ -2,10 +2,15 @@ import React, { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 import { Home } from './src/pages/Home';
 import SplashScreen from 'react-native-splash-screen';
+import codePush from "react-native-code-push";
 
-export default function App(){
+function App(){
   
   useEffect(() => {
+    codePush.sync({
+      installMode: codePush.InstallMode.IMMEDIATE
+    });
+
     SplashScreen.hide();    
   }, []);
 
@@ -16,3 +21,7 @@ export default function App(){
     </>
   )
 }
+
+export default codePush({
+  checkFrequency: codePush.CheckFrequency.ON_APP_RESUME
+})(App);
